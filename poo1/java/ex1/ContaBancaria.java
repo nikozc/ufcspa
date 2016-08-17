02d714cda03c71aec9ebe50c79894cb2cd909182
Nicolas Zachow Coelho <nicolas@nicolas.eti.br>
2016-08-17 16:25:15 -0300

    Update ContaBancaria.java

--- poo1/java/ex1/ContaBancaria.java
+++ poo1/java/ex1/ContaBancaria.java
@@ -28,10 +28,29 @@ public class ContaBancaria {
     public void mostraDados() {
         System.out.println("Nome: "+this.nomeCorrentista);
         System.out.println("Saldo: "+this.saldo);
-        System.out.println("Conta : "+this.saldo);
+        System.out.println("Conta especial : "+this.contaEspecial);
     }
 
+
+
+
+    //Isso não é uma boa ideia
     public float getSaldo() {
         return this.saldo;
+    }
+    public String getNomeCorrentista() {
+        return this.nomeCorrentista;
+    }
+    public bool getContaEspecial() {
+        return this.contaEspecial;
+    }
+    public void setSaldo( float saldo ) {
+        this.saldo = saldo;
+    }
+    public void setNomeCorrentista( String nome ) {
+        this.nomeCorrentista = nome;
+    }
+    public void setContaEspecial( bool conta ) {
+        this.contaEspecial = conta;
     }
 }
