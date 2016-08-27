ddf8fb65007d3730dd1fd219b8ec7c9a0ace11cd
Nicolas Zachow <nicolas@nicolas.eti.br>
2016-08-27 17:53:19 -0300

    Validação e Verificação de Software

---
+++ vvs/map.mm
@@ -0,0 +1,132 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1472328455836" ID="1" MODIFIED="1472328807788" TEXT="M&#xf3;dulo Paciente">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1472328455836" ID="5" MODIFIED="1472328678376" POSITION="right" TEXT="Novo Paciente">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1472328455837" FOLDED="true" ID="10" MODIFIED="1472328678377" TEXT="Dados opcionais">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472328455837" ID="15" MODIFIED="1472328487911" TEXT="Segundo nome">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" ID="16" MODIFIED="1472328487914" TEXT="Sexo">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" ID="17" MODIFIED="1472328487915" TEXT="Data de nascimento">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" ID="18" MODIFIED="1472328487917" TEXT="Nacionalidade">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" ID="19" MODIFIED="1472328487918" TEXT="Ocupa&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" ID="20" MODIFIED="1472328487919" TEXT="Status do paciente">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" ID="21" MODIFIED="1472328487920" TEXT="&quot;external patient id&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455837" FOLDED="true" ID="22" MODIFIED="1472328549293" TEXT="Tipo sanguineo">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472328455837" ID="39" MODIFIED="1472328487921" TEXT="A+"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="40" MODIFIED="1472328487922" TEXT="A-"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="41" MODIFIED="1472328487922" TEXT="AB+"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="42" MODIFIED="1472328487923" TEXT="AB-"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="43" MODIFIED="1472328487923" TEXT="B+"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="44" MODIFIED="1472328487923" TEXT="B-"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="45" MODIFIED="1472328487924" TEXT="O+"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="46" MODIFIED="1472328487924" TEXT="O-"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" ID="23" MODIFIED="1472328487925" TEXT="&quot;clinic site&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" ID="24" MODIFIED="1472328487926" TEXT="&quot;referred by&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" ID="25" MODIFIED="1472328487928" TEXT="&quot;referred date&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" ID="26" MODIFIED="1472328487929" TEXT="Religi&#xe3;o">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" ID="28" MODIFIED="1472328487930" TEXT="Respons&#xe1;vel">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" ID="29" MODIFIED="1472328487931" TEXT="Perfil de pagamento">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455838" FOLDED="true" ID="30" MODIFIED="1472328549294" TEXT="Tipo de paciente">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472328455838" ID="47" MODIFIED="1472328487933" TEXT="Particular"/>
+<node COLOR="#111111" CREATED="1472328455839" ID="48" MODIFIED="1472328487934" TEXT="Caridade"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472328455839" FOLDED="true" ID="9" MODIFIED="1472328678377" TEXT="Dados obrigat&#xf3;rios">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472328455839" ID="11" MODIFIED="1472328487938" TEXT="Id">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="12" MODIFIED="1472328487940" TEXT="Primeiro nome">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="14" MODIFIED="1472328487941" TEXT="Sobrenome">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472328455839" FOLDED="true" ID="31" MODIFIED="1472328678377" TEXT="Informa&#xe7;&#xf5;es de contato">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472328455839" ID="32" MODIFIED="1472328487947" TEXT="Telefone">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="33" MODIFIED="1472328487948" TEXT="Email">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="34" MODIFIED="1472328487950" TEXT="Endere&#xe7;o">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="35" MODIFIED="1472328487951" TEXT="Bairro">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="36" MODIFIED="1472328487953" TEXT="Cidade">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="37" MODIFIED="1472328487954" TEXT="Estado">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472328455839" ID="38" MODIFIED="1472328487955" TEXT="Pais">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1472328455839" ID="2" MODIFIED="1472328678377" POSITION="right" TEXT="Listagem de pacientes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1472328455839" ID="4" MODIFIED="1472328678377" POSITION="right" TEXT="Relat&#xf3;rios">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1472328455840" ID="6" MODIFIED="1472328678377" TEXT="Tipo de relat&#xf3;rio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1472328455840" ID="7" MODIFIED="1472328678378" TEXT="Data inicial">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1472328455840" ID="8" MODIFIED="1472328678378" TEXT="Data final">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1472328455840" ID="3" MODIFIED="1472328678378" POSITION="right" TEXT="Pacientes internados">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
